3c570cbb6738fecc534fc07e4dfb49d86171dd30
Aaron Finazzo <finazzocodes@gmail.com>
2023-10-23 06:05:43 -0400

    Implement basic calculator functionality
    - Added event listeners for number buttons, decimal button, operator
    buttons, clear button, and equals button.
    - Implemented functions for appending numbers, resetting the screen,
    clearing the screen, and appending decimals.
    - Implemented functions to set and evaluate operations.
    - Added keyboard input handling for number entry, point entry,
    evaluation, clearing, and operator selection.
    - Added error handling for division by zero.

--- script.js
+++ script.js
@@ -1,6 +1,90 @@
 let firstOperand = "";
-let operator = "";
 let secondOperand = "";
+let operator = "";
+let currentOperation = null;
+let shouldResetScreen = false;
+
+const numberBtns = document.querySelectorAll(`[data-number]`);
+const decimalBtn = document.getElementById("decimalBtn");
+const operatorBtns = document.querySelectorAll(".btn-operator");
+const clearBtn = document.getElementById("clearBtn");
+const equalsBtn = document.getElementById("equalsBtn");
+const lastOperationScreen = document.getElementById("lastOperationScreen");
+const currentOperationScreen = document.getElementById(
+  "currentOperationScreen"
+);
+
+const appendNumber = (number) => {
+  if (currentOperationScreen.textContent === "0" || shouldResetScreen) {
+    resetScreen();
+  }
+  currentOperationScreen.textContent += number;
+};
+
+const resetScreen = () => {
+  currentOperationScreen.textContent = "";
+  shouldResetScreen = false;
+};
+
+const clear = () => {
+  currentOperationScreen.textContent = "0";
+  lastOperationScreen.textContent = "";
+  firstOperand = "";
+  secondOperand = "";
+  currentOperation = null;
+};
+
+const appendDecimal = () => {
+  if (shouldResetScreen) resetScreen();
+  if (currentOperationScreen.textContent === "") {
+    currentOperationScreen.textContent = "0";
+  }
+  if (currentOperationScreen.textContent.includes(".")) return;
+  currentOperationScreen.textContent += ".";
+};
+
+const setOperation = (operator) => {
+  if (currentOperation !== null) evaluate();
+  firstOperand = currentOperationScreen.textContent;
+  currentOperation = operator;
+  lastOperationScreen.textContent = `${firstOperand} ${currentOperation}`;
+  shouldResetScreen = true;
+};
+
+const evaluate = () => {
+  if (currentOperation === null || shouldResetScreen) return;
+  if (currentOperation === "÷" && currentOperationScreen.textContent === "0") {
+    alert("You can't divide by 0!");
+    return;
+  }
+  secondOperand = currentOperationScreen.textContent;
+  currentOperationScreen.textContent = roundResult(
+    operate(currentOperation, firstOperand, secondOperand)
+  );
+  lastOperationScreen.textContent = `${firstOperand} ${currentOperation} ${secondOperand} =`;
+  currentOperation = null;
+};
+
+const roundResult = (number) => {
+  return Math.round(number * 1000) / 1000;
+};
+
+const handleKeyboardInput = (e) => {
+  if (e.key >= 0 && e.key <= 9) appendNumber(e.key);
+  if (e.key === ".") appendPoint();
+  if (e.key === "=" || e.key === "Enter") evaluate();
+  if (e.key === "Backspace") deleteNumber();
+  if (e.key === "Escape") clear();
+  if (e.key === "+" || e.key === "-" || e.key === "*" || e.key === "/")
+    setOperation(convertOperator(e.key));
+};
+
+const convertOperator = (keyboardOperator) => {
+  if (keyboardOperator === "/") return "÷";
+  if (keyboardOperator === "*") return "×";
+  if (keyboardOperator === "-") return "−";
+  if (keyboardOperator === "+") return "+";
+};
 
 const add = (a, b) => {
   return a + b;
@@ -19,12 +103,14 @@ const divide = (a, b) => {
 };
 
 const operate = (operator, a, b) => {
+  a = Number(a);
+  b = Number(b);
   switch (operator) {
     case "+":
       return add(a, b);
-    case "-":
+    case "−":
       return subtract(a, b);
-    case "*":
+    case "×":
       return multiply(a, b);
     case "÷":
       if (b === 0) {
@@ -35,3 +121,16 @@ const operate = (operator, a, b) => {
       return "Invalid Operation";
   }
 };
+
+window.addEventListener("keydown", handleKeyboardInput);
+equalsBtn.addEventListener("click", evaluate);
+clearBtn.addEventListener("click", clear);
+decimalBtn.addEventListener("click", appendDecimal);
+
+numberBtns.forEach((button) => {
+  button.addEventListener("click", () => appendNumber(button.textContent));
+});
+
+operatorBtns.forEach((button) => {
+  button.addEventListener("click", () => setOperation(button.textContent));
+});
